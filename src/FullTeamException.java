2ae0f478ef140f7524da3e939ae02eba471ef977
Shakeeb Saleh <Shakeeb8644@gmail.com>
2015-07-09 01:17:17 -0400

    Revert "Revert "version 1.0 BETA s open for testing! ""
    This reverts commit 61474d692f9d11a5e7ff61856dbfe974f7dfc1c3.

---
+++ src/FullTeamException.java
@@ -0,0 +1,18 @@
+
+public class FullTeamException extends Exception {
+
+	public FullTeamException(){
+		super();
+	}
+	
+	public FullTeamException(String message){
+		super(message);
+	}
+	
+	public FullTeamException(String message, Throwable cause){
+		super(message, cause);
+	}
+	public FullTeamException(Throwable cause){
+		super(cause);
+	}
+}
